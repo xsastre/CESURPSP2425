3b61d5bb6239a6b5ad9a3f2e4a98440d80455fd1
Xavier <34465008+xsastre@users.noreply.github.com>
2024-10-23 01:15:40 +0200

    canvis mínims

--- src/main/java/docencia/xaviersastre/dam/psp/ut02/UT2Exemple6.java
+++ src/main/java/docencia/xaviersastre/dam/psp/ut02/UT2Exemple6.java
@@ -2,47 +2,48 @@ package docencia.xaviersastre.dam.psp.ut02;
 
 import java.util.Random;
 
-class TareaCompleja2 implements Runnable{
+class TascaComplexa2 implements Runnable{
     @Override
     public void run() {
         Random generador= new Random();
-        int numAzar=(1+generador.nextInt(5))*100;
-        for (int i=0; i<numAzar;i++){
+        int numAtzar=(1+generador.nextInt(5))*100;
+        for (int i=0; i<numAtzar;i++){
             int a=i*3;
         }
-        Thread hiloActual=Thread.currentThread();
-        String miNombre=hiloActual.getName();
+        Thread filActual=Thread.currentThread();
+        String elmeuNom=filActual.getName();
         System.out.println(
-                "Finalizado el hilo"+miNombre);
+                "Finalitzat el fil"+elmeuNom
+                );
     }
 }
 public class UT2Exemple6 {
     public static void main(String[] args) {
-        int NUM_HILOS=100;
-        Thread[] hilosAsociados;
+        int NUM_FILS=100;
+        Thread[] filsAssociats;
 
-        hilosAsociados=new Thread[NUM_HILOS];
-        for (int i=0;i<NUM_HILOS;i++){
-            TareaCompleja2 t=new TareaCompleja2();
-            Thread hilo=new Thread(t);
-            hilo.setName("Hilo: "+i);
-            hilo.start();
-            hilosAsociados[i]=hilo;
+        filsAssociats=new Thread[NUM_FILS];
+        for (int i=0;i<NUM_FILS;i++){
+            TascaComplexa2 t=new TascaComplexa2();
+            Thread fil=new Thread(t);
+            fil.setName("fil: "+i);
+            fil.start();
+            filsAssociats[i]=fil;
         }
 
-        /* Despues de crear todo, nos aseguramos
-         * de esperar que todos los hilos acaben. */
+        /* Després de crear-ho tot, ens asseguram
+         * d'esperar que tots els fils acabin. */
 
-        for (int i=0; i<NUM_HILOS; i++){
-            Thread hilo=hilosAsociados[i];
+        for (int i=0; i<NUM_FILS; i++){
+            Thread fil=filsAssociats[i];
             try {
-                //Espera a que el hilo acabe
-                hilo.join();
+                //Espera a que el fil acabe
+                fil.join();
             } catch (InterruptedException e) {
-                System.out.print("Algun hilo acabó ");
-                System.out.println(" antes de tiempo!");
+                System.out.print("Algun fil ha acabat ");
+                System.out.println(" abans de temps!");
             }
         }
-        System.out.println("El principal ha terminado");
+        System.out.println("El principal ha acabat");
     }
 }
