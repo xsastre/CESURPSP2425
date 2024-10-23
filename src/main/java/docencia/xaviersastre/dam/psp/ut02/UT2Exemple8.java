e8a1e2d3e685c5405690f12366c4c2d1406b6c31
Xavier <34465008+xsastre@users.noreply.github.com>
2024-10-23 02:32:40 +0200

    Update UT2Exemple8.java

--- src/main/java/docencia/xaviersastre/dam/psp/ut02/UT2Exemple8.java
+++ src/main/java/docencia/xaviersastre/dam/psp/ut02/UT2Exemple8.java
@@ -1,21 +1,24 @@
 package docencia.xaviersastre.dam.psp.ut02;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class UT2Exemple8 extends Thread {
     public void run() {
 System.out.println("Dins del fil:" + this.getName() +
         ", prioritat: "+this.getPriority() +
-        " ID: " + this.getId() );
+        " ID: " + this.threadId());
     }
     //
     public static void main(String[] args) {
         UT2Exemple8 h = null;
-        for (int i=0; i < 3; i++) {
+        for (int i=0; i < 5; i++) {
             h = new UT2Exemple8();  //crear fil
             h.setName("FIL" + i);   //posam nom al fil
-            h.setPriority(i + 1);   //posam prioritat
+            h.setPriority(ThreadLocalRandom.current().nextInt(0, 10));   //posam prioritat
             h.start();              //iniciar fil
             System.out.println("Informació del " +
                     h.getName() + ": " + h.toString());
         }
-            System.out.println("3 FILS CREATS...");
+            System.out.println("5 FILS CREATS...");
     }   // main
 }       // clase
